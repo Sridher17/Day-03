e4a0ec59e5400e95056130f5a0969740cb0586d3
Sridher17 <124970018+Sridher17@users.noreply.github.com>
2023-02-20 00:09:45 +0530

    Add files via upload

---
+++ JSON Format Resume.js
@@ -0,0 +1,14 @@
+var Resume ={
+    "Name":"Sridher D C",
+    "AGE":23,
+    "date of birth":17/05/2000,
+    "Address":"No 1035 TNHB Avadi Chennai 600054",
+    "Qulification":"Diploma in Mechanical Engineering",
+    "Marks In Percentage":86,
+    "institute Name":"Murugappa Polytechnic College",
+    "Experience":"Nil",
+}
+for(var a in Resume){
+    console.log(a,Resume[a]);
+    
+}
---
+++ JSON for in loop.js
@@ -0,0 +1,8 @@
+var studentname={
+    "name":"Sridher",
+    "Age":23,
+    "address":"Chennai",
+}
+for(var a in studentname){
+    console.log(a,studentname[a]);
+}
